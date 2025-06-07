1e59700f219a3b949eb5b8ebedc4dc1c32c5b6f9
Josephine <jbl.noetic@gmail.com>
2025-06-07 23:29:54 +0000

    hangman with pygame

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -6,6 +6,12 @@
 
 # INITIALIZING ###################################################################################################
 import random # a built-in module for random choice
+import pygame
+
+pygame.init() # initialize pygame and
+screen = pygame.display.set_mode((800, 600)) # create a new display
+screen.fill(0)
+pygame.display.set_caption("HANGMAN")
 
 MAX_INCORRECT_GUESSES = 6 # create a global variable containing the max number of incorrect guesses
 
@@ -17,22 +23,26 @@ MAX_INCORRECT_GUESSES = 6 # create a global variable containing the max number o
 def selectWord() -> str:
     file = open("hangmanWords.txt", "r") # open the file
     fileContent = file.read() # read from the file
-    wordsList = fileContent.splitlines() # split the content based on the lines
+    wordsList = fileContent.split() # split into a list
     file.close()
     return random.choice(wordsList) # return a random word from the list
 
-# Function to get the player input
+# Function to get the player input and check validity
 # @param: guessedLetters - a list containing the characters the user has guessed already
 #               this list is passed by reference, meaning any changes we make to it in the function
 #               will be recorded outside this function
+# @param: playerInput - a character containing the user input received from pygame; 
+#                this parameter will be added in week 4
 # @return: string - representing a valid user letter guess 
-def getPlayerInput(guessedLetters:list) -> str:
+def getPlayerInput(guessedLetters:list, playerInput:chr) -> str:
     while True: # keep getting the user input until it is a new, valid guess
-        playerInput = input("Guess a letter: ")
-        playerInput = playerInput.lower() # make sure to lowercase it
+        # playerInput = input("Guess a letter: ") # this was implemented in week 3 but we get rid of it because of pygame!
         if len(playerInput) == 1 and playerInput.isalpha() and (playerInput not in guessedLetters): # player has not guessed this letter yet
+            playerInput = playerInput.lower() # make sure to lowercase it
             guessedLetters.append(playerInput) # add it to the guessedLetters list
             return playerInput
+        # else - invalid guess
+        return "" # so just return a blank character
 
 # Function to display how much of the secret word that the user has guessed already
 # @param: secretWord - the string containing the secret word to guess
@@ -45,16 +55,18 @@ def buildWordGuess(secretWord:str, guessedLetters:list) -> str:
         if (letter in guessedLetters): # if that letter has been guessed already
             currentWord += str(letter) # add it to the string we want to return
         else: # otherwise
-            currentWord += "_" # add an underscore
+            currentWord += "-" # add a dash
     return currentWord
 
 # Function to draw the hangman
 # @param: wrongGuesses - the number (integer) representing how many guesses the user has gotten wrong
 # @return: void
 def drawHangman(wrongGuesses:int):
+    # empty the drawing by covering it with a rectangle
+    pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, 800, 300)) 
     hangman = [ # storing the drawings of the different phases of hangman
 r"""
-------
+______
  |   |
  |   
  |  
@@ -62,7 +74,7 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |  
@@ -70,7 +82,7 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |   |
@@ -78,7 +90,7 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |  /|
@@ -86,7 +98,7 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |  /|\
@@ -94,7 +106,7 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |  /|\
@@ -102,14 +114,22 @@ r"""
  |________
 """,
 r"""
-------
+______
  |   |
  |   O
  |  /|\
  |  / \
  |________
 """]
-    print(hangman[wrongGuesses]) # print out the hangmand depending on how many wrong guesses the user has had
+    # print out the hangman depending on how many wrong guesses the user has had
+    # break it up per line since render only renders one line at a time
+    lineNum = 0
+    for line in hangman[wrongGuesses].split('\n'):
+        print(line)
+        label = hangmanDrawing.render(line, 1, (255,255,255))
+        screen.blit(label, (300, 100+(35*lineNum))) # draw at these coords x,y
+        lineNum += 1
+    pygame.display.flip() # update the display
     return # return nothing
 
 # Function to check when the game is over and print whether the user won
@@ -121,11 +141,17 @@ r"""
 #               False = the game is not over yet 
 def gameOver(wrongGuesses:int, secretWord:str, guessedLetters:list) -> bool:
     print("\n")
+    pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 350, 800, 300)) # empty the screen
+    gameStatus = pygame.font.Font(None, 75)
     if (wrongGuesses >= MAX_INCORRECT_GUESSES): # the player guesses wrong too many times
         print("You lose!")
+        gameStatusLabel = gameStatus.render("You lose!", 1, (255,0,0), (0,0,0))
+        screen.blit(gameStatusLabel, (300, 450))
         return True
     if set(secretWord) <= set(guessedLetters): # all the letters in the secret word have already been guessed
         print("You win!!!")
+        gameStatusLabel = gameStatus.render("You win!!!", 1, (0,255,0), (0,0,0))
+        screen.blit(gameStatusLabel, (300, 450))
         return True
     return False
 
@@ -140,16 +166,65 @@ secretWord = selectWord()
 guessedLetters = []
 wrongGuesses = 0
 isGameOver = False
+userGuess = ""
 
-drawHangman(wrongGuesses); # draw the initial hangman
-while not isGameOver: # keep going until the game is over!
-    print(buildWordGuess(secretWord, guessedLetters)) # printing the progress of the word being guessed
-    userGuess = getPlayerInput(guessedLetters)
-    if userGuess not in secretWord:
-        wrongGuesses += 1 # increment the number of wrong guesses
-    print("Guessed letters: " + str(sorted(guessedLetters))) # print the list of guesses the player has already guessed
-    drawHangman(wrongGuesses); # draw the hangman
-    isGameOver = gameOver(wrongGuesses, secretWord, guessedLetters) # set the variable to determine if the game is over
 
-# game is over so reveal the secret word!
-print("The secret word was " + secretWord)
+# NEW STUFF WITH PYGAME
+hangmanDrawing = pygame.font.Font(None, 50)
+wordDrawing = pygame.font.Font(None, 50)
+guessesDrawing = pygame.font.Font(None, 35)
+
+# draw the initial hangman and text
+drawHangman(wrongGuesses); 
+secretWordText = buildWordGuess(secretWord, guessedLetters)
+print(secretWordText)
+wordLabel = wordDrawing.render(secretWordText, 1, (255,255,255), (0,0,0))
+screen.blit(wordLabel, (300, 50))
+pygame.display.flip() # update the display
+
+while True: # loop for pygame
+    # event handling -- nothing too major
+    for event in pygame.event.get():
+        # keep going until the game is over!
+        # or if we manually exit the game
+        if isGameOver or event.type == pygame.QUIT:
+            # game is over so reveal the secret word!
+            drawHangman(wrongGuesses) # draw hangman one last time
+            
+            # let the user know what the secret word is
+            print("The secret word was " + secretWord)
+            wordLabel = wordDrawing.render(secretWord, 1, (255,255,255), (0,0,0))
+            screen.blit(wordLabel, (300, 50))
+
+            pygame.display.flip() # update the display
+
+            pygame.time.wait(1000) # wait 1 second before closing the game
+            pygame.quit() # quit pygame
+
+        # detect user guess
+        if event.type == pygame.KEYDOWN:
+            userGuess = event.unicode # get the key pressed
+
+            userGuess = getPlayerInput(guessedLetters, userGuess) # validate user guess
+            if userGuess != "": # if user guess valid
+                if userGuess not in secretWord: 
+                    wrongGuesses += 1 # increment the number of wrong guesses
+            # else: # user guess invalid so do nothing
+            
+            drawHangman(wrongGuesses); # draw the hangman
+
+            # printing the progress of the word being guessed
+            secretWordText = buildWordGuess(secretWord, guessedLetters)
+            print(secretWordText)
+            wordLabel = wordDrawing.render(secretWordText, 1, (255,255,255), (0,0,0))
+            screen.blit(wordLabel, (300, 50))
+
+            # print the list of guesses the player has already guessed
+            print("Guessed letters: " + str(sorted(guessedLetters))) 
+            guessesLabel = guessesDrawing.render(str(sorted(guessedLetters)), 1, (255,255,255), (0,0,0))
+            screen.blit(guessesLabel, (200, 450))
+
+            pygame.display.flip() # update the display
+
+            # set the variable to determine if the game is over
+            isGameOver = gameOver(wrongGuesses, secretWord, guessedLetters) 
